ddca4ce314c8ffcc3b87e9429b092a035615bd72
Nicolas-lab2424 <Conigaleano4@gmail.com>
2025-08-11 17:03:56 -0300

    add:funcionalidad al register

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,52 +1,53 @@
 import { Layout } from "../components/Layout"
-
+import { useState } from "react"
 const Register = () => {
+
+  const [username, setUsername] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  const handleUsername = (e) => {
+    setUsername(e.target.value)
+    console.log("hola")
+  }
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value)
+
+  }
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value)
+  }
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(username, email, password)
+  }
+
 
   return (
 
     <Layout>
       <div>
         <h1>Registrarse</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label htmlFor="nombre">Nombre:</label>
-            <input
-              type="text"
-              id="nombre"
-              name="nombre"
-              placeholder="Ingrese su nombre"
-            />
+            <input onChange={handleUsername} type="text" id="nombre" name="nombre" placeholder="Ingrese su nombre" />
           </div>
 
           <div>
             <label htmlFor="email">Correo electrónico:</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              placeholder="Ingrese su correo"
-            />
+            <input onChange={handleEmail} type="email" id="email" name="email" placeholder="Ingrese su correo" />
           </div>
 
           <div>
             <label htmlFor="password">Contraseña:</label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              placeholder="Ingrese su contraseña"
-            />
+            <input onChange={handlePassword} type="password" id="password" name="password" placeholder="Ingrese su contraseña" />
           </div>
 
-          <div>
-            <label htmlFor="confirmPassword">Confirmar contraseña:</label>
-            <input
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              placeholder="Confirme su contraseña"
-            />
-          </div>
 
           <button type="submit">Crear cuenta</button>
         </form>
