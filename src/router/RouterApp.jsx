c61ad15c11042a88649cbde73e0dd0b4008f983c
Nicolas-lab2424 <Conigaleano4@gmail.com>
2025-08-11 14:19:16 -0300

    Add:Termine la RouterApp

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,6 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { Home } from "../pages/Home"
 import { Login } from "../pages/Login"
+import { Dashboard } from "../pages/Dashboard"
+import { NotFound } from "../pages/NotFound"
+import { Register } from "../pages/Register"
+
+
 // 1. BrowserRouter habilita el historial de navegación del navegador, (posibilidad de leer el path de una URL).
 // 2. Routes es quien contiene el conjunto de rutas.
 // 3. Route define la ruta (path='/') exacta de un componente (element={<Componente />}).
@@ -12,6 +17,9 @@ const RouterApp = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/Dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
